a078cbf35e36af808277fd099cd62a5304ec94f3
aloproducao <43450424+aloproducao@users.noreply.github.com>
2020-07-14 03:26:16 -0300

    Update client.js

--- public/client.js
+++ public/client.js
@@ -19,9 +19,8 @@ var iceServers = {
     ]
 }
 var streamConstraints = { audio: true, video: true,
-                        video: {
-    width: { min: 427, ideal: 1280, max: 1920 },
-    height: { min: 240, ideal: 720, max: 1080 }
+                                                  { width:  min: 427, ideal: 1280, max: 1920 },
+    { height:  min: 240, ideal: 720, max: 1080 }
                         };
                          
                          
